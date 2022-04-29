aaa3fd1f9680a33d5707b5d677fdcddeb1902f1b
Maji <33150916+M4gie@users.noreply.github.com>
2022-04-29 17:46:37 +0200

    :sparkles: feat: scripts to deploy on npm (#14)

--- packages/ui/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts}"],
-  theme: {
-    extends: {},
-  },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
