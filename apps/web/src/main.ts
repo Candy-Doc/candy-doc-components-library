44200efc3b0bd31322a09d19e1c3e89e539ff6c5
Maji <steeven.regnault@outlook.fr>
2022-04-29 14:37:31 +0200

    refractor: remove monorepo architecture to keep only the component library

--- apps/web/src/main.ts
+++
@@ -1,9 +0,0 @@
-import App from "./App.svelte";
-import "./styles/tailwind.css";
-import "./styles/main.css";
-
-const app = new App({
-  target: document.getElementById("app"),
-});
-
-export default app;
