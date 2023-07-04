ae60ec667594797a08247e30c788c7d2a7314230
AntonyDerache <antony.derache@epitech.eu>
2023-07-04 10:25:34 +0200

    Revert ":pencil: refactor: rename popover's Types.js to types.ts"
    This reverts commit 4704888709cb2431f94454f108e159b75ae0c45d.

--- src/components/Popover/Popover.ts
+++ src/components/Popover/Popover.ts
@@ -1,7 +1,7 @@
 import { LitElement, PropertyValues, html } from "lit";
 import { customElement, property, query } from "lit/decorators.js";
 
-import { PopoverSide } from "./types";
+import { PopoverSide } from "./Types";
 import PopoverStyle from "./PopoverStyle";
 
 export type CandyPopoverProps = {
--- src/components/Popover/index.ts
+++ src/components/Popover/index.ts
@@ -1,3 +1,3 @@
 import "./Popover";
 export { type CandyPopoverProps } from "./Popover";
-export { PopoverSide } from "./types";
+export { PopoverSide } from "./Types";
